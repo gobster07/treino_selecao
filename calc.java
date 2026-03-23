d5fa2340dc8d9290aec2d58fb96c3a8933fa0510
gobster07 <139504591+gobster07@users.noreply.github.com>
2026-03-23 18:01:33 -0300

    Add files via upload

--- calc.java
+++ calc.java
@@ -15,34 +15,33 @@ public class calc
         Scanner in = new Scanner(System.in);
         
         double v1, v2, ans;
-        int op;
-        
+        String mod;
         System.out.println("Informe a operação");
-        System.out.println(" 1 = +");
-        System.out.println(" 2 = -");
-        System.out.println(" 3 = *");
-        System.out.println(" 4 = /");
-        System.out.println(" 5 = %");
-        op = in.nextInt();
+        System.out.println("+");
+        System.out.println("-");
+        System.out.println("*");
+        System.out.println("/");
+        System.out.println("%");
+        mod = in.nextLine();
         System.out.println("Informe valor 1");
         v1 = in.nextDouble();
         System.out.println("Informe valor 2");
         v2 = in.nextDouble();
+        char op = mod.charAt(0);
         
-        
-        if(op == 1){
+        if(op == '+'){
             ans = v1 + v2;
             System.out.println(ans);
-        } else if(op == 2){
+        } else if(op == '-'){
             ans = v1 - v2;
             System.out.println(ans);
-        } else if(op == 3){
+        } else if(op == '*'){
             ans = v1 * v2;
             System.out.println(ans);
-        } else if(op == 4){
+        } else if(op == '/'){
             ans = v1 / v2;
             System.out.println(ans);
-        } else if(op == 5){
+        } else if(op == '%'){
             ans = v1 % v2;
             System.out.println(ans);
         } 
